e336e047de62c6a8d277e147716d70994898975c
Stephen Foster <stephenfostercpp@gmail.com>
2022-11-07 14:41:57 -0800

    Enemy AI Pathing + Terrain NavMesh
    Added Enemy AI Pathing to Enemy.cs
    It required Brackeys NavMeshComponent Extensions that have been added to Resources/External
    
    Prefab of Enemy was created
    Prefab of Player was created with PlayerManager as a child (for things like always knowing its position, persistent info)
    All set up in Test World.scene

--- IntoTheHorde/Assets/Scripts/Enemy.cs
+++ IntoTheHorde/Assets/Scripts/Enemy.cs
@@ -1,12 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
+    public float lookRadius = 10f;
     private EnemySpawning enemySpawning;
-
+    Transform target;
+    NavMeshAgent agent;
+    private void Start()
+    {
+        target = PlayerManager.instance.player.transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
     // Make sure this gets called when player attacks enemy
+
     public void TakeDamage()
     {
         Destroy(gameObject);
@@ -17,5 +27,35 @@ public class Enemy : MonoBehaviour
         {
             enemySpawning.spawnerDone = true;
         }
+    }
+    private void Update()
+    {
+        float distance = Vector3.Distance(target.position, transform.position);
+        if(distance <= lookRadius)
+        {
+            agent.SetDestination(target.position);
+            if(distance <= agent.stoppingDistance)
+            {
+                //attack
+                //Right now there's no transform for "front" that keeps in mind sprite direction or player direction (think sphere at the tip of the weapon)
+                //Once there is and it's standardized into a prefab, we can look into facing
+
+                //face target probably will need to be offset by the 90 degress in either direction?
+                //FaceTarget();
+                //lets try this for when we create attacking ability first
+                //transform.LookAt(target, Vector3.left);
+            }
+        }
+    }
+    private void FaceTarget()
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 0.5f);
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
     }
 }
