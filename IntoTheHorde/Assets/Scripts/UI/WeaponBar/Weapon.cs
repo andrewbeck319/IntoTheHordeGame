c2ac044b156eb88096c826a940bfe95d1b4f40ce
Michał Obara <mobaradev@yahoo.com>
2022-11-28 02:45:52 -0800

    Weapon bar scripts added
    but not finished unfortunatetly

---
+++ IntoTheHorde/Assets/Scripts/UI/WeaponBar/Weapon.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UI
+{
+    public class Weapon : MonoBehaviour
+    {
+        public bool IsInCooldown = false;
+        public float CooldownTimeRemaining;
+    }
+   
+}
---
+++ IntoTheHorde/Assets/Scripts/UI/WeaponBar/WeaponBar.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponBar : MonoBehaviour
+{
+    public List<UI.Weapon> Weapons = new List<UI.Weapon>();
+    public UI.Weapon WeaponSelected;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
